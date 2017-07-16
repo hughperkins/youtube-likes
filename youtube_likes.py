556fe42056ee34c1a1caa607a3cd5dffb5cfdc88
Hugh Perkins <hughperkins@gmail.com>
2017-07-16 12:46:27 -0400

    add number subscriptions

--- youtube_likes.py
+++ youtube_likes.py
@@ -15,45 +15,46 @@ def run(config_file):
     api_key = config['api_key']
     channel_id = config['channel_id']
 
-    # res = requests.get('https://www.googleapis.com/youtube/v3/channels/?mine=true'
-    #                                                '&part=contentDetails'
-    #                                                '&key={api_key}'.format(api_key=api_key))
-    # assert res.status_code == 200
-    # d = json.loads(res.content)
+    persisted = {}
+
+    res = requests.get('https://www.googleapis.com/youtube/v3/channels/?id={channel_id}'
+                       '&part=statistics'
+                       '&key={api_key}'.format(
+                            channel_id=channel_id,
+                            api_key=api_key))
+    if res.status_code != 200:
+        print('res.status_code %s' % res.status_code)
+        print(res.content)
+        raise Exception('invalid status code %s' % res.status_code)
+    d = json.loads(res.content)
     # print(json.dumps(d, indent=2))
-    # return
-    res = requests.get('https://www.googleapis.com/youtube/v3/activities/?maxResults=25'
+    persisted['num_subscriptions'] = d['items'][0]['statistics']['subscriberCount']
+    # print('num_subscriptions %s')
+    res = requests.get(
+        'https://www.googleapis.com/youtube/v3/activities/?maxResults=25'
         '&channelId={channel_id}'
         '&part=snippet%2CcontentDetails'
         '&key={api_key}'.format(api_key=api_key, channel_id=channel_id))
-    # print('res.status_code', res.status_code)
     if res.status_code != 200:
         print('res.status_code %s' % res.status_code)
         print(res.content)
     assert res.status_code == 200
-    # print(res.content)
     d = json.loads(res.content.decode('utf-8'))
-    # print(json.dumps(d, indent=2))
     videos = []
     for item in d['items']:
-        # id = item['id']
         title = item['snippet']['title']
         video_id = item['contentDetails']['upload']['videoId']
         videos.append({'title': title, 'video_id': video_id})
-        # print(title, video_id)
 
-    # video_ids 
-    # video_ids = ['MKA6v99uYKY']
     res = requests.get('https://www.googleapis.com/youtube/v3/videos/?id={video_ids}'
                        '&part=snippet%2CcontentDetails%2Cstatistics'
                        '&key={api_key}'.format(
                             video_ids=','.join([v['video_id'] for v in videos]),
                             api_key=api_key))
-    # print('res.status_code', res.status_code)
     assert res.status_code == 200
     d = json.loads(res.content.decode('utf-8'))
-    # print(json.dumps(d, indent=2))
     videos = []
+    persisted['videos'] = videos
     for item in d['items']:
         video_id = item['id']
         title = item['snippet']['title']
@@ -75,11 +76,18 @@ def run(config_file):
     # print(json.dumps(videos, indent=2))
     if path.isfile(config['cache_file']):
         with open(config['cache_file'], 'r') as f:
-            old_stats = yaml.load(f)
+            old_persisted = yaml.load(f)
+            if 'videos' not in old_persisted:
+                old_persisted = {
+                    'videos': old_persisted,
+                    'num_subscriptions': 0
+                }
     else:
-        old_stats = []
+        # old_stats = []
+        old_persisted = {'videos': [], 'num_subscriptions': 0}
+    old_videos = old_persisted['videos']
     old_by_id = {}
-    for video in old_stats:
+    for video in old_videos:
         old_by_id[video['video_id']] = video
     new_by_id = {}
     for video in videos:
@@ -100,11 +108,15 @@ def run(config_file):
                 if old_video[k] != video[k]:
                     output += '  %s %s => %s\n' % (k, old_video[k], video[k])
             if output != '':
-                print(video['title'] + ':')
+                print((video['title'] + ':').encode('utf-8'))
                 print(output[:-1])
 
+    if persisted['num_subscriptions'] != old_persisted['num_subscriptions']:
+        print('num subscriptions: %s => %s' % (
+            old_persisted['num_subscriptions'], persisted['num_subscriptions']))
+
     with open(config['cache_file'], 'w') as f:
-        yaml.dump(videos, f)
+        yaml.dump(persisted, f)
 
 
 if __name__ == '__main__':
