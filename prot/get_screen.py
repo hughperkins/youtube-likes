805d45ad7e2e399b92b47f37f12ea4e702ac4a1f
Hugh Perkins <hughperkins@gmail.com>
2025-01-18 14:07:08 -0500

    add time period to get_screen.py

--- prot/get_screen.py
+++ prot/get_screen.py
@@ -4,6 +4,7 @@ refresh page, filter on 'scre', right click the get_screen request, and do 'copy
 - pasted contents into /tmp/request.js
 """
 import argparse
+import datetime
 import json
 from typing import Any, cast
 import requests
@@ -15,6 +16,13 @@ JSON = dict[str, 'JSON'] | list['JSON'] | str | int | None | float
 join_url = "https://studio.youtube.com/youtubei/v1/yta_web/join?alt=json"
 join_body_str = "{\"nodes\":[{\"key\":\"0__TOTALS_SUMS_QUERY_KEY\",\"value\":{\"query\":{\"dimensions\":[],\"metrics\":[{\"type\":\"LIKES_PER_LIKES_PLUS_DISLIKES_PERCENT\",\"includeTotal\":true},{\"type\":\"RATINGS_LIKES\",\"includeTotal\":true},{\"type\":\"RATINGS_DISLIKES\",\"includeTotal\":true}],\"restricts\":[{\"dimension\":{\"type\":\"VIDEO\"},\"inValues\":[\"HI5sjIr9gak\"]}],\"orders\":[],\"timeRange\":{\"dateIdRange\":{\"inclusiveStart\":20241221,\"exclusiveEnd\":20250118}},\"currency\":\"USD\",\"returnDataInNewFormat\":true,\"limitedToBatchedData\":false}}},{\"key\":\"1__TOTALS_TIMELINE_QUERY_KEY\",\"value\":{\"query\":{\"dimensions\":[{\"type\":\"DAY\"}],\"metrics\":[{\"type\":\"LIKES_PER_LIKES_PLUS_DISLIKES_PERCENT\"}],\"restricts\":[{\"dimension\":{\"type\":\"VIDEO\"},\"inValues\":[\"HI5sjIr9gak\"]}],\"orders\":[{\"dimension\":{\"type\":\"DAY\"},\"direction\":\"ANALYTICS_ORDER_DIRECTION_ASC\"}],\"timeRange\":{\"dateIdRange\":{\"inclusiveStart\":20241221,\"exclusiveEnd\":20250118}},\"currency\":\"USD\",\"returnDataInNewFormat\":true,\"limitedToBatchedData\":false}}}],\"connectors\":[],\"allowFailureResultNodes\":true,\"context\":{\"client\":{\"clientName\":62,\"clientVersion\":\"1.20250115.02.00\",\"hl\":\"en\",\"gl\":\"US\",\"experimentsToken\":\"\",\"utcOffsetMinutes\":-300,\"rolloutToken\":\"CLa8vteVgvPM2wEQw8mfzZCWigMYrdHBoZ7_igM%3D\",\"userInterfaceTheme\":\"USER_INTERFACE_THEME_DARK\",\"screenWidthPoints\":1470,\"screenHeightPoints\":474,\"screenPixelDensity\":2,\"screenDensityFloat\":2},\"request\":{\"returnLogEntry\":true,\"internalExperimentFlags\":[],\"eats\":\"Ac1K1YyqRgpIwfyBrUqDTmCFarp3+10mrRbvwMp7mb9Cc+tShOEfV4MfxTDf6hfPD+ZixjGFdcXS6pynPkCyO+ywSF0cWMtzSBCWMyg=\",\"sessionInfo\":{\"token\":\"YR1SZJKFZR\"},\"consistencyTokenJars\":[]},\"user\":{\"onBehalfOfUser\":\"108940515231048981095\",\"delegationContext\":{\"externalChannelId\":\"UCHPoNHk_aC5LmJDDVAH4w7Q\",\"roleType\":{\"channelRoleType\":\"CREATOR_CHANNEL_ROLE_TYPE_OWNER\"}},\"serializedDelegationContext\":\"EhhVQ0hQb05Ia19hQzVMbUpERFZBSDR3N1EqAggI\"},\"clientScreenNonce\":\"PIdrdlQgDFFE02Vs\"},\"trackingLabel\":\"web_explore_video\"}"
 join_body = json.loads(join_body_str)
+
+analytics_time_period_by_days = {
+    7: "ANALYTICS_TIME_PERIOD_TYPE_WEEK",
+    28: "ANALYTICS_TIME_PERIOD_TYPE_FOUR_WEEKS",
+    90: "ANALYTICS_TIME_PERIOD_TYPE_QUARTER",
+    365: "ANALYTICS_TIME_PERIOD_TYPE_YEAR",
+}
 
 
 class Json:
@@ -95,9 +103,10 @@ def try_Json_class(res):
     print('impressions', impressions, 'views', views, f'ctr {ctr:.1f}')
 
 
-def load_tab(url: str, headers: Any, body: Any, tab_name: str, video_id: str) -> Json:
+def load_tab(url: str, headers: Any, body: Any, tab_name: str, video_id: str, days: int) -> Json:
     screen_config = body["screenConfig"]
     screen_config["entity"]["videoId"] = video_id
+    screen_config["timePeriod"]["timePeriodType"] = analytics_time_period_by_days[days]
     # print(json.dumps(screen_config, indent=2))
     desktop_state = body["desktopState"]
     # ANALYTICS_TAB_ID_REACH
@@ -136,6 +145,22 @@ def get_join_result(join_json_results, join_key: str):
     raise Exception(f"Could not find join result {join_key}")
 
 
+def render_join_body(join_body, video_id: str, days: int):
+    # print(json.dumps(join_body, indent=2))
+    for node in join_body["nodes"]:
+        restricts = node["value"]["query"]["restricts"]
+        # print('restrict', json.dumps(restricts, indent=2))
+        restricts[0]["inValues"][0] = video_id
+        date_id_range = node["value"]["query"]["timeRange"]["dateIdRange"]
+        # print("date_id_range", date_id_range)
+        end_str = datetime.date.today().strftime("%Y%m%d")
+        start_str = (datetime.date.today() - datetime.timedelta(days=days)).strftime("%Y%m%d")
+        # print('start_str', start_str, 'end_str', end_str)
+        date_id_range["inclusiveStart"] = start_str
+        date_id_range["exclusiveEnd"] = end_str
+    return join_body
+
+
 def run(args) -> None:
     with open(args.get_screen_js) as f:
         query_raw = f.read()
@@ -146,7 +171,9 @@ def run(args) -> None:
     body = json.loads(body_str)
     url = query_raw.partition(",")[0].split('"')[1]
 
-    join_res = requests.post(join_url, headers=headers, data=join_body_str)
+    join_body_rendered = render_join_body(join_body, video_id=args.video_id, days=args.days)
+
+    join_res = requests.post(join_url, headers=headers, data=json.dumps(join_body_rendered))
     if join_res.status_code >= 300:
         raise Exception("failed", join_res.status_code, join_res.content)
     # print(join_res.status_code)
@@ -178,8 +205,10 @@ def run(args) -> None:
     #     return
     # try_Json_class(res)
 
-    reach_tab = load_tab(url=url, headers=headers, body=body, tab_name="ANALYTICS_TAB_ID_REACH", video_id=args.video_id)
-    engagement_tab = load_tab(url=url, headers=headers, body=body, tab_name="ANALYTICS_TAB_ID_ENGAGEMENT", video_id=args.video_id)
+    reach_tab = load_tab(
+        url=url, headers=headers, body=body, tab_name="ANALYTICS_TAB_ID_REACH", video_id=args.video_id, days=args.days)
+    engagement_tab = load_tab(
+        url=url, headers=headers, body=body, tab_name="ANALYTICS_TAB_ID_ENGAGEMENT", video_id=args.video_id, days=args.days)
 
     impressions = get_metric_total3(reach_tab, "VIDEO_THUMBNAIL_IMPRESSIONS")
     ctr = get_metric_total3(reach_tab, "VIDEO_THUMBNAIL_IMPRESSIONS_VTR")
@@ -196,7 +225,8 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--get-screen-js', default="/tmp/request.js", type=str)
     parser.add_argument('--video-id', required=True, type=str)
-    parser.add_argument('--time-period', choices=["ANALYTICS_TIME_PERIOD_TYPE_FOUR_WEEKS"])
+    # parser.add_argument('--time-period', choices=["ANALYTICS_TIME_PERIOD_TYPE_FOUR_WEEKS"])
+    parser.add_argument('--days', required=True, type=int, choices=analytics_time_period_by_days.keys())
     args = parser.parse_args()
     run(args)
 
